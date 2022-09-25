444839ec042058b0a2ff22899a1e5f6c419ea8ce
SimvolokovP <psimvolokov@gmail.com>
2022-09-25 22:00:08 +0300

    Задание 3

---
+++ Zadaniye3/ex9.py
@@ -0,0 +1,13 @@
+def add():
+    n = int(input("Кол-во долек в длину: "))
+    m = int(input("Кол-во долек в ширину: "))
+    k = int(input("Кол-во долек: "))
+    if k < n * m:
+        if (k % n == 0) or (k % m == 0):
+            print("Да")
+        else:
+            print("Нет")
+    else:
+        print("Некорректные значения")  
+    return add          
+add()            
