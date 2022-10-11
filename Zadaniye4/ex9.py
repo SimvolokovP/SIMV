db79e22829b4b170bb6c6df5bfd7a8ff7c7545a9
SimvolokovP <psimvolokov@gmail.com>
2022-10-11 22:26:44 +0300

    Задание 4

--- Zadaniye4/ex9.py
+++ Zadaniye4/ex9.py
@@ -1,8 +1,12 @@
-n = int(input("Кол-во чисел из ряда Фибоначчи: ")) 
-def add(x):
-    c = 1
-    p = 0
-    while (c < x):
-        c, p = c+p, c
-    return (c+p-1)
-print("Сумма чисел равна: ", add(n))    
+n = int(input("Введите число рядов Фибоначчи: "))
+def add(x): 
+    fib1 = fib2 = 1 
+    print(fib1)
+    print(fib2)
+    s = fib1 + fib2
+    for i in range(2, n):
+        fib1, fib2 = fib2, fib1 + fib2
+        s += fib2
+        print(fib2)
+    return(s)
+print("Сумма:",add(n))    
