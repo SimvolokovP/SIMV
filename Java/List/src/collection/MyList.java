57f241b2af3e6ec9c972a9802075a2736262446c
SimvolokovP <113377637+SimvolokovP@users.noreply.github.com>
2023-10-28 09:46:22 +0300

    CRUD

--- Java/List/src/collection/MyList.java
+++
@@ -1,9 +0,0 @@
-package collection;
-
-public interface MyList {
-     void add(int index, int number);
-     void remove(int index);
-     int getSize();
-     int get(int index);
-     void clear();
-}
